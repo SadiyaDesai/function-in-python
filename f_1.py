a101cc7603b30c10ad107ed729671e44ff0e46f6
SadiyaDesai <117843916+SadiyaDesai@users.noreply.github.com>
2022-11-13 18:32:11 +0400

    funtion_1

---
+++ f_1.py
@@ -0,0 +1,89 @@
+
+"""
+
+def add(a,b):
+    print(a+b)
+a=12
+b=80
+print(a+b)
+add(a,b)
+add(2,3)
+add(100,300)
+.........................................
+x=10
+y=20
+def add(x,y):
+
+    print(x+y)
+add(x,y)
+a=10
+b=20
+x=20
+add(a,b)
+add(b,x)
+.......................................
+
+def add(a,b):
+    print(a+b)
+a = 3
+b = 4
+add(a,b)
+from math import pi
+print(pi)
+add(10,40)
+add(300,400)
+
+
+def fruits(a,b,c):
+    print(a,b,c)
+a="apple"
+b="banana"
+c="chickoo"
+fruits(a,b,c)
+import math as m
+print(m.sqrt(6))
+fruits(a,b,c)
+......................................................................
+def maxi(x,y):
+
+    if x>y:
+        print("max val",x)
+x=60
+y=20
+maxi(x,y)
+maxi(100,50)
+maxi(50,30)
+.........................................
+
+def add(a,b,c):
+    print(a+b+c)
+add(10,20,30)
+add(1000,30,2)
+a=5
+b=6
+c=12
+add(a,b,c)
+import math as m
+print(m.pi)
+..............................
+def fam(m_n,f_n,c_n):
+    print("hello",m_n," ",f_n)
+    print("hi",f_n)
+    print("how are u",c_n)
+fam("sadiya","Mudassar","khadija")
+"""
+
+s=[11,12,13,55]
+l=len(s)
+print(l)
+s=sum(s)
+print("sum of items",s)
+
+
+
+
+
+
+
+
+
